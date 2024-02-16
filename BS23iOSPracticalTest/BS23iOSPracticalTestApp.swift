7b9a8e693dffc967345de36052e96d871626f974
Mac Mini <>
2024-02-16 09:58:01 +0600

    Project Created

---
+++ BS23iOSPracticalTest/BS23iOSPracticalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BS23iOSPracticalTestApp.swift
+//  BS23iOSPracticalTest
+//
+//  Created by mac 2019 on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BS23iOSPracticalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
